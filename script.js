660f2552fb3663e40323d0b0fe7ce46ba6eb6313
brendamanon0526@gmail <Brenda_Manon@Administrators-MacBook-Pro.local>
2023-10-11 09:05:46 -0400

    logical operators

---
+++ script.js
@@ -0,0 +1,64 @@
+//Logical Operators = are symbols or words used to write more complex conditions, its usually
+//  to connect two or more expressions.  
+
+// && and -is used to check if both conditions are true
+// || or -checks if at least 1 condition is true
+//! not -reverses the boolean value
+
+
+
+
+
+//check the temp and display a message.
+
+// //want it to fall in a certain range!!!
+
+// var temperature = 70; //in fahrenheit
+
+// if(temperature > 86){
+//     console.log("It's HOT outside!");
+// }                                    //both have to be true for statement to be true
+
+// // else if(temperature > 32 && temperature <= 86){
+// //     console.log("It is warm outside!");
+// // }
+// else{
+//     console.log("It's COLD outside!");
+// };
+
+
+
+
+
+///Check if it's dark or light outside
+
+
+// var hour = 20; //military time 8:00pm 
+
+// if(hour < 6 || hour >= 20){
+//     console.log("It's Dark outside!");
+// }
+// else{
+//     console.log("It's LIGHT outside!")
+// }
+
+
+
+
+
+
+
+
+// var myName = window.prompt("Enter your name");
+
+// if(myName == ""){
+//     console.log("You did not enter your name!");
+// }
+// //                                 ///check if name is not an empty string
+// if(!(myName == "")){
+    
+//     console.log("Hello", myName);
+// }
+// else{
+//     console.log("You did not enter your name!");
+//  }
